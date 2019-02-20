c905541e259aed6805c8f6f88d17c5a80ccb3523
LightSkin <herzleiid@gmail.com>
2019-02-20 17:12:04 -0300

    Fixes/revert

--- 8-TPs/jsAgilidadAritmetica2.js
+++ 8-TPs/jsAgilidadAritmetica2.js
@@ -5,13 +5,68 @@ se agregara un temporizador que
  correcto en ese lapso de tiempo. */
 var respuesta;
 var temporizador;
-function comenzar()
-{
-	
-}//FIN DE LA FUNCIÓN
-function Responder()
-{
-	
+var numero1;
+var numero2;
+var operador;
+var check;
 
+function comenzar() {
+    numero1 = document.getElementById("PrimerNumero").value = RandomNumber(1, 10);
+    numero2 = document.getElementById("SegundoNumero").value = RandomNumber(1, 10);
+    switch (RandomNumber(1, 4)) {
+        case 1:
+            AssignOperator("*");
+            break;
+        case 2:
+            AssignOperator("/");
+            break;
+        case 3:
+            AssignOperator("-");
+            break;
+        default:
+            AssignOperator("+");
+            break;
+    }
+    setTimeout(function Perdiste() { if (check) alert("TIME OUT!!"); }, 5000);
+}
 
-}//FIN DE LA FUNCIÓN
+function Responder() {
+    respuesta = document.getElementById("Respuesta").value;
+    check = true;
+    switch (operador) {
+        case "*":
+            if (respuesta == numero1 * numero2)
+                alert("Correcto!!!")
+            else
+                alert("Incorrecto.")
+            break;
+        case "/":
+            if (respuesta == numero1 / numero2)
+                alert("Correcto!!!")
+            else
+                alert("Incorrecto.")
+            break;
+        case "-":
+            if (respuesta == numero1 - numero2)
+                alert("Correcto!!!")
+            else
+                alert("Incorrecto.")
+            break;
+        case "+":
+            if (respuesta == numero1 + numero2)
+                alert("Correcto!!!")
+            else
+                alert("Incorrecto.")
+            break;
+        default:
+            break;
+    }
+}
+
+function AssignOperator(op) {
+    operador = document.getElementById("Operador").value = op;
+}
+
+function RandomNumber(min, max) {
+    return Math.floor(Math.random() * max + min);
+} 
